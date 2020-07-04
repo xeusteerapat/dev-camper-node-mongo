ff59764ae83ff31c03ee77b03b196d24c2a3e2d5
xeusteerapat <xeus085@gmail.com>
2020-07-04 16:00:17 +0700

    get all  bootcamps and single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,21 +3,36 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc  Get all bootcamps
 // @routes  GET /api/v1/bootcamps
 // @access Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: 'List all bootcamps',
-  });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: bootcamps,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  Get single bootcamps
 // @routes  GET /api/v1/bootcamps/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `Get single bootcamp with id ${req.params.id}`,
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  Create single bootcamps
