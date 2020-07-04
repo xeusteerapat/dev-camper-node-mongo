e45e22dd7cabba68f44cfccb62f5a4b3b84f29fa
xeusteerapat <xeus085@gmail.com>
2020-07-04 15:03:03 +0700

    setup mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 require('dotenv').config({
   path: './config/config.env',
 });
+
+// Connect MongoDB
+connectDB();
 
 const app = express();
 
@@ -18,8 +23,17 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `🔥Server is running in ${process.env.NODE_ENV} mode on port ${PORT}🔥`
+      .green.bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
 });
