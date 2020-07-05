b96b52c1cddb71ec2380c9f8e7caba1ea80d9d4a
xeusteerapat <xeus085@gmail.com>
2020-07-05 11:45:13 +0700

    add seeder for bulk import and delete data

---
+++ seeder.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+require('dotenv').config({
+  path: './config/config.env',
+});
+
+const Bootcamp = require('./models/Bootcamp');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import to DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data deleted...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
