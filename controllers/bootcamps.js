4f763ed48d4adaf5967fef05be952566e607dd89
xeusteerapat <xeus085@gmail.com>
2020-07-06 15:04:06 +0700

    update cascade deletion

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -131,13 +131,16 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @routes  DELETE /api/v1/bootcamps/:id
 // @access Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Delete after found
+  bootcamp.remove();
 
   res.status(200).json({
     success: true,
