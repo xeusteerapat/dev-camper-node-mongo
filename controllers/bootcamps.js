25153e43458ef78ece12f9d362418818a7d96efc
xeusteerapat <xeus085@gmail.com>
2020-07-06 14:58:52 +0700

    update virtuals and populate data relation

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,7 +24,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select Fields
   if (req.query.select) {
