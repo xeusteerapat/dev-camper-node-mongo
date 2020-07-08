916cf92f0c82b2036d0cfe9a5c3a5a9a39ed0c4e
xeusteerapat <xeus085@gmail.com>
2020-07-08 14:57:34 +0700

    refactor auth middleware authorize function

--- middleware/auth.js
+++ middleware/auth.js
@@ -33,17 +33,15 @@ exports.protect = asyncHandler(async (req, res, next) => {
 });
 
 // Grant access to specific role
-exports.authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorResponse(
-          `User role ${req.user.role} is unauthorized to access this route`,
-          403
-        )
-      );
-    }
+exports.authorize = (...roles) => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return next(
+      new ErrorResponse(
+        `User role ${req.user.role} is unauthorized to access this route`,
+        403
+      )
+    );
+  }
 
-    next();
-  };
+  next();
 };
